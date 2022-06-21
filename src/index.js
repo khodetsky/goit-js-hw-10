4a51fa9c559aaf689869167377ee01cf0d863dc8
khodetsky <hodyaya@gmail.com>
2022-06-21 17:46:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputFeild = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list')
+
+// fetch(`https://restcountries.com/v3.1/name/por?fields=name.official,capital,population,flags,languages`)
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(country => {
+//         console.log(country)
+//     });
+
+
+function fetchCountries(evt) {
+    const name = evt.currentTarget.value;
+        fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        return response.json();
+    }).then(countries => {
+
+        const marcup = countries.map((country) => {
+            `<li>
+            <img href="${country.flags.svg}">
+            <p>${country.name.official}</p>
+            </li>`
+        }).join("");
+
+        countryList.insertAdjacentHTML("beforeend", marcup)
+        // createCountryMarcup(countries);
+        // console.log(countriesTpl(countries));
+        console.log(countries)
+    });
+};
+
+
+
+
+function createCountryMarcup(countries) {
+    countryList.insertAdjacentHTML("beforeend", countriesTpl(countries))
+};
+
+function countriesTpl(countries) {
+    const marcup = countries.map((country) => {
+        `<li>
+        <img href="${country.flags.svg}">
+        <p>${country.name.official}</p>
+        </li>`
+    }).join("");
+    return marcup;
+};
+
+inputFeild.addEventListener('input', fetchCountries)
